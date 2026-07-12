fd325b6a2ea40c174fbc95bbabc049c594f35bdd
obnull <ermasukd@gmail.com>
2026-07-12 14:59:09 +0300

    feat: created thread script

---
+++ 05-python-api/day-138-threading/main.py
@@ -0,0 +1,18 @@
+import threading
+
+numbers = [1, 2, 3]
+
+def func(int):
+    print(f"Working with {int}")
+
+threads = []
+
+for number in numbers:
+    thread = threading.Thread(target=func, args=(number, ))
+    threads.append(thread)
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
